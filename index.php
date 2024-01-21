b3789dea4b48c002faf4e1f3a89a5aedc67848cd
Nicholas Roberts <82165867+trissentety@users.noreply.github.com>
2024-01-21 12:59:05 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+include("database.php")
+
+//How to connect
+//Via MySQL i Extension or PDO (PHP Data Objects). PDO Allows connecting to more than one database.
+
+//If using xampp, open control panel 
+//> make sure module sql is started 
+//> admin (http://localhost/phpmyadmin/) 
+//> database tab
+//> choose database name & create database
+//> click 3 lines
+//> user accounts
+//> information used is hostname, user name of root and password
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    Greetings<br>
+</body>
+
+</html>
+
+<!--view main web page http://localhost/website/ -->
